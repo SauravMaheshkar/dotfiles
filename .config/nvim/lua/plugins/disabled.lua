e0b25499153240235148fa8fc6a6bb87f1bf950c
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2024-12-29 10:28:23 +0000

    feat: update nvim config

---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "windwp/nvim-ts-autotag",
+    enabled = false,
+  },
+  {
+    "nvim-lua/plenary.nvim",
+    enabled = false,
+  },
+}
---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      dashboard = { enabled = true, example = "doom" },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      scroll = { enabled = true },
+      statuscolumn = { enabled = true },
+      words = { enabled = true },
+    },
+  },
+}
